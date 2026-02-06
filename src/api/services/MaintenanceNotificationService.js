7653eaa08ea90b86c678e16b4e01b6b20631c814
Jesús Félix Oliva Ballestero <liconterapelopo@gmail.com>
2026-02-06 19:24:23 +0100

    Fix maintenance inbox loading

--- src/api/services/MaintenanceNotificationService.js
+++ src/api/services/MaintenanceNotificationService.js
@@ -1,14 +1,15 @@
-import { request } from "../axiosClient";
+import Config from "../../config/apiConfig";
+import { buildParams, request } from "../axiosClient";
 
 const MaintenanceNotificationService = {
   getInbox() {
     return request({
       url: Config.VEHICLES.SEARCH, 
       method: "GET",
-      params: { 
+      params: buildParams({
         vehicleStatusId: 2,
-        pageSize: 50       
-      }
+        pageSize: 50
+      })
     });
   }
 };
