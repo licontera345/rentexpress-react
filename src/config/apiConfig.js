a8e5ccda5879ff18db17c797df0b34c0f998b8a7
licontera345 <liconterapelopo@gmail.com>
2026-02-12 20:53:49 +0100

    modified:   src/config/apiConfig.js

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,6 +1,7 @@
 const Config = {
     // URL base del backend REST.
-    API_BASE_URL: "https://94.130.104.92:8443/rentexpress-rest-api/api",
+   // API_BASE_URL: "https://94.130.104.92:8443/rentexpress-rest-api/api",
+   API_BASE_URL: "http://localhost:8081/rentexpress-rest-api/api",
 
     AUTH: {
         // Endpoints de autenticación para usuarios y empleados.
