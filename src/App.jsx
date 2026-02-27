62aac1e98e5b958142f678884b969ad6fe6cbbe6
licontera345 <liconterapelopo@gmail.com>
2026-02-27 21:31:38 +0100

    Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.jsx 	modified:   src/main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import useLocale from './hooks/core/useLocale';
 import useAuth from './hooks/core/useAuth';
 import AuthAxiosSetup from './components/auth/AuthAxiosSetup';
 import ErrorBoundary from './components/common/ErrorBoundary';
+//import { GoogleLogin } from '@react-oauth/google';
 
 function AppContent() {
   const { logout } = useAuth();
@@ -19,13 +20,42 @@ function AppContent() {
 }
 
 function App() {
-  useLocale();
-
+   useLocale();
   return (
-    <BrowserRouter basename="/">
-      <AppContent />
+     <BrowserRouter basename="/">
+       <AppContent />
     </BrowserRouter>
   );
 }
 
 export default App;
+
+
+
+/* <GoogleLogin
+    onSuccess={async (credentialResponse) => {
+      const idToken = credentialResponse.credential;
+
+      try {
+        const response = await fetch(
+          "http://localhost:8081/rentexpress-rest-api/api/open/auth/google",
+          {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ token: idToken }),
+          }
+        );
+        console.log("HTTP Status:", response.status);
+
+        // If backend returns JSON
+        const data = await response.json();
+        console.log("Response body:", data);
+
+      } catch (error) {
+        console.error("Fetch error:", error);
+      }
+    }}
+    onError={() => console.log("Login Failed")}
+  /> */
+
+
--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { AuthProvider } from './context/AuthContext';
 import './styles/app.css';
+//import { GoogleOAuthProvider } from '@react-oauth/google';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+
+    // <GoogleOAuthProvider clientId="983385335826-4gcf6skskeh4votp94gbdeo5se6us97g.apps.googleusercontent.com">
+    //     <App />
+    // </GoogleOAuthProvider>
     <React.StrictMode>
         <AuthProvider>
             <App />
